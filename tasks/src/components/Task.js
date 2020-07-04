d44f94dfba379f64b4d2967a4880ca4a787bc1f8
Alesandro Facina Balani <afbalani@hotmail.com>
2020-07-04 11:15:54 -0300

    filtrando as tasks done

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import commomStyles from '../CommomStyles'
 import moment from 'moment'
@@ -15,9 +15,12 @@ export default props => {
         
     return(
         <View style={styles.container}>
-            <View style={styles.checkContainer}>
-                {getCheckView(props.doneAt)}
-            </View>
+            <TouchableWithoutFeedback
+                onPress={() => props.toogleTask(props.id)}>
+                <View style={styles.checkContainer}>
+                    {getCheckView(props.doneAt)}
+                </View>
+            </TouchableWithoutFeedback>
             <View style={styles.taskContainer}>
                 <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
                 <Text style={[styles.date, doneOrNotStyle]}>{dateFormated}</Text>
--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -4,99 +4,133 @@ import {
     , Text
     , ImageBackground
     , StyleSheet
-    , FlatList } from 'react-native'
+    , FlatList
+    , TouchableOpacity
+    , Platform } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import TodayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commomStyles from '../CommomStyles'
 import Task from '../components/Task'
 
+const initialTasks = [
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    },
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    },
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    },
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    },
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    },
+    {
+        id: Math.random()
+        , desc: 'Task Um'
+        , estimateAt: new Date()
+    }
+    , {
+        id: Math.random()
+        , desc: 'Task Dois'
+        , estimateAt: new Date()
+        , doneAt: new Date()
+    }
+]
+
 export default class TaskList extends Component {
     state = {
-        tasks: [
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            },
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            },
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            },
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            },
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            },
-            {
-                id: Math.random()
-                , desc: 'Task Um'
-                , estimateAt: new Date()
-            }
-            , {
-                id: Math.random()
-                , desc: 'Task Dois'
-                , estimateAt: new Date()
-                , doneAt: new Date()
-            }
-        ]
+        showDoneTasks: true        
+        ,tasks: [...initialTasks]
+        , visibleTasks: [...initialTasks]
     }
+
+    toogleTask = id => {
+        const tasks = [...this.state.tasks]
+        const taskFiltered = tasks.filter(t => t.id == id)[0]
+        taskFiltered.doneAt = taskFiltered.doneAt ? null : new Date()
+        this.setState({ tasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = [...this.state.tasks]
+        if (!this.state.showDoneTasks)
+            visibleTasks = this.state.tasks.filter(t => t.doneAt == null)
+        this.setState({ visibleTasks })
+    }
+
+    toggleDoneItems = () => {            
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
     render() {
         const today = moment().locale('pt-br').format('dddd, D [de] MMMM [de] YYYY')
         return(
             <View style={styles.container}>
                 <ImageBackground style={styles.background} source={TodayImage}>
+                    <View style={styles.iconBar}> 
+                        <TouchableOpacity onPress={this.toggleDoneItems}>
+                            <Icon 
+                                name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20}
+                                color={commomStyles.colors.secundary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subTitle}>{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         style={styles.flatList}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} />} />
+                        renderItem={({item}) => <Task {...item} toogleTask={this.toogleTask} />} />
                 </View>
             </View>
         )
@@ -133,5 +167,11 @@ const styles = StyleSheet.create({
     }
     , flatList: {
         flex: 1
+    }
+    , iconBar: {
+        flexDirection: 'row'        
+        , marginHorizontal: 20
+        , justifyContent: 'flex-end'
+        , marginTop: Platform.OS === 'ios' ? 40 : 10
     }
 })
